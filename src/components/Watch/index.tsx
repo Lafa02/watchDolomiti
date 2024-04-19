3eb79352da33926f8f7821014ba4f5d821434e6a
Nicolò Lafasciano <nicololafasciano@gmail.com>
2024-04-19 11:01:27 +0200

    feat: completed watch

--- src/components/Watch/index.tsx
+++ src/components/Watch/index.tsx
@@ -2,70 +2,70 @@ import classNames from "classnames";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import style from "./style.module.scss";
-import { useRef, useState } from "react";
-import { motion } from "framer-motion";
+import { useMemo, useState } from "react";
 
 const Watch = () => {
   const [startingPosition, setStartingPosition] = useState({ x: 0, y: 0 });
   const [centerPoint, setCenterPoint] = useState({ x: 0, y: 0 });
   const [angle, setAngle] = useState(0);
-  const ref = useRef<HTMLDivElement | null>(null);
+  const actualAngle = useMemo(() => Math.round(angle / 15) * 15, [angle]);
+  const [isDragging, setIsDragging] = useState(false);
 
-  const handleDragStart = (e: MouseEvent) => {
-    if (!(e instanceof PointerEvent)) return;
-    console.log(e);
+  const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    const emptyImg = new Image();
+    e.dataTransfer.setDragImage(emptyImg, 0, 0);
+    e.dataTransfer.dropEffect = "move";
+    setIsDragging(true);
     setStartingPosition({
-      x: e.clientX,
-      y: e.clientY,
+      x: e.nativeEvent.clientX,
+      y: e.nativeEvent.clientY,
     });
-    if (!e.target) return;
+    if (!e.currentTarget) return;
     setCenterPoint({
-      x: e.offsetX + (ref.current?.clientWidth || 0) / 2,
-      y: e.offsetY + (ref.current?.clientHeight || 0) / 2,
+      x: e.currentTarget.offsetLeft + (e.currentTarget.clientWidth || 0) / 2,
+      y: e.currentTarget.offsetTop + (e.currentTarget?.clientHeight || 0) / 2,
     });
   };
 
-  const handleDragEnter = (e: PointerEvent) => {
-    e.preventDefault();
-    const currentPosition = {
-      x: e.offsetX,
-      y: e.offsetY,
-    };
-    const sAngle = Math.atan2(
-      startingPosition.y - centerPoint.y,
-      startingPosition.x - centerPoint.x
-    );
-    const pAngle = Math.atan2(
-      currentPosition.y - centerPoint.y,
-      currentPosition.x - centerPoint.x
-    );
+  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
+    e.dataTransfer.dropEffect = "move";
+    if (
+      isDragging &&
+      e.nativeEvent.clientX !== 0 &&
+      e.nativeEvent.clientY !== 0
+    ) {
+      const currentPosition = {
+        x: e.nativeEvent.clientX,
+        y: e.nativeEvent.clientY,
+      };
+      const sAngle = Math.atan2(
+        startingPosition.y - centerPoint.y,
+        startingPosition.x - centerPoint.x
+      );
+      const pAngle = Math.atan2(
+        currentPosition.y - centerPoint.y,
+        currentPosition.x - centerPoint.x
+      );
 
-    console.log(currentPosition, startingPosition);
+      console.log(currentPosition);
 
-    setAngle((ond) => ond + ((pAngle - sAngle) * 180) / Math.PI);
-    setStartingPosition(currentPosition);
+      setAngle((ond) => ond + ((pAngle - sAngle) * 180) / Math.PI);
+      setStartingPosition(currentPosition);
+    }
   };
 
   return (
-    <motion.div
-      drag
-      ref={ref}
-      animate={{
-        rotate: `${angle}deg`,
+    <div
+      draggable
+      onDrag={(e) => handleDragEnter(e)}
+      style={{
+        transform: `rotate(${actualAngle}deg)`,
       }}
-      onDrag={(e) => handleDragEnter(e as PointerEvent)}
-      onMouseMove={(e) => handleDragStart(e as unknown as MouseEvent)}
-      dragElastic={0}
-      dragConstraints={{
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-      }}
+      onDragStart={(e) => handleDragStart(e as React.DragEvent<HTMLDivElement>)}
       className={classNames(style.containerWatch)}
     >
-      <CircularProgressbar value={4} maxValue={24} />
-    </motion.div>
+      <CircularProgressbar strokeWidth={13} value={4} maxValue={24} />
+    </div>
   );
 };
 
